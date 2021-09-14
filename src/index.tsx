af27023b248c71f4b6343476e6d64282686c7558
VladyslavHimself <vladyslavhimself@gmail.com>
2021-09-14 17:27:58 -0400

    changed eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 
-const application = (
-  <App />
-);
-
-ReactDOM.render(application, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
