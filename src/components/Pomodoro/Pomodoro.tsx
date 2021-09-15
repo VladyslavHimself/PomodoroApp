fee9b3ea7e05f7b72a7d06e3ade85952580777fa
VladyslavHimself <vladyslavhimself@gmail.com>
2021-09-15 17:10:26 -0400

    added navigation

--- src/components/Pomodoro/Pomodoro.tsx
+++ src/components/Pomodoro/Pomodoro.tsx
@@ -5,12 +5,21 @@ import classes from "./pomodoro.module.scss";
 import Header from "../Ui/Header/Header";
 import Button from "../Ui/Button/Button";
 
+import statistics from '../../assets/statistics.svg';
+
 type Props = {
   
 };
 export const Pomodoro = (props: Props) => {
   return (
     <div className={classes.pomodoro}>
+      <div className={classes.navigation}>
+        <div className={classes.statistics}>
+          <a href="/">
+            <img src={statistics} alt="statistics"/>
+          </a>
+        </div>
+      </div>
       <div className={classes.panel}>
         <Header title={'Pomodoro'} />
         <div className={classes.timer}>
